9d9a9046a2ff9704cc96bd635aba5d0124032e09
Luca Mimmo <171295375+LucaMimmo05@users.noreply.github.com>
2026-01-06 20:07:27 +0100

    feat: Implement ProjectCard component and integrate project mock data in Projects page

---
+++ src/components/ui/ProjectCard.tsx
@@ -0,0 +1,102 @@
+import { ArrowRight, Ellipsis } from "lucide-react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./card";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./dropdown-menu";
+import { Button } from "./button";
+import PriorityBadge from "./PriorityBadge";
+import { Badge } from "./badge";
+import { Separator } from "./separator";
+
+type ProjectProps = {
+  project: {
+    id: number;
+    name: string;
+    description: string;
+    status: string;
+    tasksCount: number;
+    progress: number;
+    membersNumber: number;
+  };
+};
+const ProjectCard = ({ project }: ProjectProps) => {
+  return (
+    <Card className="xl:min-w-120 min-w-100 flex-1 lg:max-w-[calc(50%-1rem)]">
+      <CardHeader className="flex  flex-row justify-between items-center pb-3">
+        <div className="flex flex-row gap-4 items-center">
+          <img
+            className="rounded-lg"
+            width={80}
+            height={80}
+            src="http://placehold.co/80x80"
+            alt=""
+          />
+          <div className="space-y-1">
+            <CardTitle>{project.name}</CardTitle>
+            <CardDescription className="hidden sm:block md:texty-base text-sm">
+              {project.description}
+            </CardDescription>
+          </div>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="p-3" variant={"outline"}>
+              <Ellipsis size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            <DropdownMenuGroup>
+              <DropdownMenuItem>Project Settings</DropdownMenuItem>
+              <DropdownMenuItem>Manage members</DropdownMenuItem>
+            </DropdownMenuGroup>
+            <DropdownMenuSeparator />
+            <DropdownMenuSeparator />
+            <DropdownMenuGroup>
+              <DropdownMenuItem className="text-yellow-600">
+                Archive Project
+              </DropdownMenuItem>
+              <DropdownMenuItem className="text-red-600">
+                Delete Project
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </CardHeader>
+
+      <CardContent className="space-x-3 pb-2">
+        <PriorityBadge data={project.status}>{project.status}</PriorityBadge>
+        <Badge variant="outline" className="hover:bg-inset text-xs shrink-0">
+          {project.tasksCount} Tasks
+        </Badge>
+      </CardContent>
+      <CardFooter className="flex-col">
+        <Separator />
+        <div className="flex justify-between items-center w-full">
+          <p className="text-muted-foreground text-sm">12 december 2026</p>
+          <Button
+          variant="link"
+          size="sm"
+          className="hover:bg-inset cursor-pointer px-2 ml-auto"
+        >
+          Go To {project.name}
+          <ArrowRight className="" size={16} />
+        </Button>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default ProjectCard;
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,7 +1,81 @@
-const Projects = () => {
-  return (
-    <div>Projects</div>
-  )
-}
+import ProjectCard from "@/components/ui/ProjectCard";
+import PageContainer from "@/layouts/PageContainer";
 
-export default Projects
+const Projects = () => {
+  const projectMock = [
+    {
+      id: 1,
+      name: "Website Redesign",
+      description: "Redesign the corporate website for better UX.",
+      status: "In Progress",
+      tasksCount: 12,
+      progress: 65,
+      membersNumber: 5,
+    },
+    {
+      id: 2,
+      name: "Mobile App Launch",
+      description: "Launch the new mobile application on iOS and Android.",
+      status: "Completed",
+      tasksCount: 8,
+      progress: 100,
+      membersNumber: 3,
+    },
+    {
+      id: 3,
+      name: "Marketing Campaign",
+      description: "Execute the summer marketing campaign.",
+      status: "Pending",
+      tasksCount: 15,
+      progress: 20,
+      membersNumber: 7,
+    },
+    {
+      id: 4,
+      name: "Customer Feedback Analysis",
+      description: "Analyze customer feedback for product improvements.",
+      status: "In Progress",
+      tasksCount: 10,
+      progress: 50,
+      membersNumber: 4,
+    },
+    {
+      id: 5,
+      name: "Cloud Migration",
+      description: "Migrate services to the cloud infrastructure.",
+      status: "Pending",
+      tasksCount: 20,
+      progress: 10,
+      membersNumber: 6,
+    },
+    {
+      id: 6,
+      name: "Security Audit",
+      description: "Conduct a comprehensive security audit.",
+      status: "Completed",
+      tasksCount: 5,
+      progress: 100,
+      membersNumber: 2,
+    },
+    {
+      id: 7,
+      name: "Security Audit",
+      description: "Conduct a comprehensive security audit.",
+      status: "Completed",
+      tasksCount: 5,
+      progress: 100,
+      membersNumber: 2,
+    },
+  ];
+  return (
+    <PageContainer>
+      <div className="flex flex-wrap gap-6 w-full">
+        {projectMock.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
+    </PageContainer>
+  );
+};
+
+export default Projects;
