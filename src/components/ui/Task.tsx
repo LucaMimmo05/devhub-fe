274573b4587d8ac707322d692b6c366395feda0c
Luca Mimmo <171295375+LucaMimmo05@users.noreply.github.com>
2026-01-10 23:09:57 +0100

    feat: Update Task component to use projectMock for project names and refine task type definitions

--- src/components/ui/Task.tsx
+++ src/components/ui/Task.tsx
@@ -1,3 +1,4 @@
+import { projectMock } from "@/mock/dashboard-mock";
 import { Checkbox } from "./checkbox";
 import PriorityBadge from "./PriorityBadge";
 
@@ -5,29 +6,27 @@ export type TaskProps = {
   task: {
     id: number;
     title: string;
-    status: string;
-    priority: string; // "High" | "Medium" | "Low";
-    createdAt: string;
-    updatedAt: string;
-    project: string;
+    status: "Pending" | "In Progress" | "Completed";
+    priority: "Low" | "Medium" | "High";
+    createdAt: Date;
+    updatedAt: Date;
+    project: number;
   };
   key: number;
 };
 
 const Task = ({ task }: TaskProps) => {
+  const project = projectMock.find((project)=> project.id === task.project)
   return (
     <div key={`${task.id}`} className="flex items-start gap-3">
       <Checkbox className="rounded mt-2 shrink-0" />
       <div className="min-w-0 flex-1">
         <div className="flex flex-wrap justify-between items-center gap-2">
-          <h3 className="text-md font-normal truncate">
-            {task.title}
-          </h3>
+          <h3 className="text-md font-normal truncate">{task.title}</h3>
           <PriorityBadge data={task.priority}>{task.priority}</PriorityBadge>
-          
         </div>
         <p className="text-xs md:text-sm text-muted-foreground">
-          {task.project}
+          {project?.name}
         </p>
       </div>
     </div>
