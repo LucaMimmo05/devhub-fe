c0a2fbb2cd24c5ca52d559b7868e295383e1c999
Luca Mimmo <171295375+LucaMimmo05@users.noreply.github.com>
2026-01-10 23:15:14 +0100

    fix: Adjust layout and spacing in NotFound component for better responsiveness

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -5,12 +5,12 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <PageContainer className="min-h-screen relative">
+    <PageContainer className="min-h-screen h-screen">
       <div className="absolute top-6 left-6">
         <Logo size="sm" />
       </div>
 
-      <div className="flex justify-center md:text-start md:justify-start items-center min-h-screen px-12 gap-12">
+      <div className="flex justify-center md:text-start md:justify-start items-center px-12 gap-12 h-full">
         <div className="flex flex-col gap-4 items-start">
           <h1 className="xl:text-[16rem] md:text-9xl text-7xl font-bold text-accent leading-none">
             404
@@ -22,7 +22,7 @@ const NotFound = () => {
             The page you are looking for does not exist.
           </p>
 
-          <Button className="mt-6 w-full md:w-30"><Link to="/">Go Home</Link></Button>
+          <Button className="mt-1 w-full md:w-30"><Link to="/">Go Home</Link></Button>
         </div>
       </div>
     </PageContainer>
